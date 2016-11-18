ab818fcb090f32a6bd9baf0e6570ba360711a25e
Jacobo García Urbano <jacobo@jackurbano.es>
2016-11-18 19:52:49 +0100

    tercer commit

--- symfony/src/AppBundle/Controller/PruebasController.php
+++ symfony/src/AppBundle/Controller/PruebasController.php
@@ -11,11 +11,27 @@ class PruebasController extends Controller
     /**
      * @Route("/pruebas/index", name="pruebasIndex")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, $name, $page)
     {
+      $productos = [
+        ["producto" => "Consola 1", "precio" => 1],
+        ["producto" => "Consola 2", "precio" => 2],
+        ["producto" => "Consola 3", "precio" => 3],
+        ["producto" => "Consola 4", "precio" => 4],
+        ["producto" => "Consola 5", "precio" => 5],
+        ["producto" => "Consola 6", "precio" => 6],
+      ];
+
+      $fruta = ["Manzana" => "Manzana", "Pera" => "Pera", "Piña" => "Piña",];
+      //return $this->redirect($request->getBaseUrl()."/hello-world?hola=true");
+    /*  var_dump($request->query->get("hola"));
+      var_dump($request->get("hola-post"));
+      die();*/
       // replace this example code with whatever you need
         return $this->render('AppBundle:Pruebas:index.html.twig', [
-          'texto' => "hola mundo"
+          'texto' => " <-> hola mundo - " . $name . ' - '. $page,
+          'productos' => $productos,
+          'fruta' => $fruta
         ]);
     }
 
